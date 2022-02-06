6020ff5778d4cb10c50d2480c5c15a761d1446a2
Shazad Hoang <shazad.hoang@gmail.com>
2022-02-06 16:42:09 +0100

    Add product card

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Product from "./Product";
+
+function ProductFeed({ products }) {
+    // console.log(products, "****");
+    return (
+        <div>
+            <h1>Products ....</h1>
+            {products.map(
+                ({ id, title, price, description, category, image }) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                    />
+                )
+            )}
+        </div>
+    );
+}
+
+export default ProductFeed;
